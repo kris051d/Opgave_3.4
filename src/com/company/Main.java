5cd163fd7ea0f39cafac4d29f1ff22451e1003e5
kris051d <kris051d@edu.easj.dk>
2021-09-07 13:43:27 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Random;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Opgave 3.4
+        System.out.println("I want to play a game :D.");
+        System.out.println("I'm thinking of a number beetween 1 and 100, can you guess it?");
+        System.out.println("Choose your number beewtween 1 and 100");
+
+        Scanner scanner = new Scanner (System.in);
+        int userNumber = scanner.nextInt();
+        System.out.println("Your number is "+userNumber);
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        System.out.print("The number i was thinking of was: ");
+        System.out.println(number);
+
+        int difference;
+        difference=(userNumber-number);
+        if (difference < 0) {difference = difference * -1;}
+
+        System.out.println("You were off by: "+difference);
+    }
+}
